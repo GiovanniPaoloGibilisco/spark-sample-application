810ef3c1c56caf5c74845a6b8d53c5cb9ad08190
GiovanniPaoloGibilisco <gibbo87@gmail.com>
2016-01-13 15:21:16 +0100

    added log4j configuration, merged the stages into one application

--- TwoStages/src/main/java/it/polimi/spark/twostages/TwoStages.java
+++ TwoStages/src/main/java/it/polimi/spark/twostages/TwoStages.java
@@ -5,8 +5,11 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TwoStages {
+	static final Logger logger = LoggerFactory.getLogger(TwoStages.class);
 
 	public static void main(String[] args) {
 		if (args.length < 3) {
@@ -17,12 +20,13 @@ public class TwoStages {
 		String input2 = args[1];
 		String output = args[2];
 		String factorS = args[3];
-		
+
 		double factor = Double.parseDouble(factorS);
-		
+
 		SparkConf conf = new SparkConf().setAppName("Two Stages");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 
+		// load the first RDD
 		JavaRDD<String> data1 = sc.textFile(input1);
 		JavaRDD<Double> doubleVect1 = data1.map(new Function<String, Double>() {
 			@Override
@@ -31,6 +35,7 @@ public class TwoStages {
 			}
 		});
 
+		// load the second RDD
 		JavaRDD<String> data2 = sc.textFile(input2);
 		JavaRDD<Double> doubleVect2 = data2.map(new Function<String, Double>() {
 			@Override
@@ -39,12 +44,25 @@ public class TwoStages {
 			}
 		});
 
+		// do the linear computation on the union of the two rdds
+		JavaRDD<Double> unitedData = doubleVect1.union(doubleVect1);
+		unitedData.map(new Function<Double, Double>() {
+			@Override
+			public Double call(Double arg0) throws Exception {
+				for (int i = 0; i < factor; i++)
+					Math.tan(arg0);
+				return Math.tan(arg0);
+			}
+		});
+		logger.info("Liner Done");
+
+		// do the cartesian product
+		JavaPairRDD<Double, Double> cartesian = doubleVect1.cartesian(doubleVect2);
+		logger.info("Cartesian Done");
 		
-		
-		
-		JavaPairRDD<Double, Double> cartesian = doubleVect1
-				.cartesian(doubleVect2);
-		cartesian.saveAsTextFile(output);
+		//perform some dummy operation including both RDDs
+		long count = cartesian.keys().union(unitedData).count();
+		logger.info("Count: "+count);
 
 	}
 
